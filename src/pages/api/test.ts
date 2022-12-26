99597e47a672f02beb974d195e626281a9772579
akiogitgit <akimaron2828@icloud.com>
2022-12-26 14:30:35 +0900

    chore: envKeyを反映させる

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -4,9 +4,9 @@ import { openai } from '../../libs/openai'
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   const completion = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: `今からあなたは小説を書きます。タイトルは「ファイアウォールとファミリーマート」です。「剛田たけし」の単語使って作成して下さい。続きが気になり非常に面白く、会話の多い小説にして下さい。`,
+    prompt: `今からあなたは小説を書きます。タイトルは「ファイアウォールとファミリーマート」です。 の単語使って作成して下さい。続きが気になり非常に面白く、会話の多い小説にして下さい。`,
     temperature: 0.6,
-    max_tokens: 300,
+    max_tokens: 100,
   })
 
   res.status(200).json({
