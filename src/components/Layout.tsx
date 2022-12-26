7c7a705eb3c6bf1bf41216836dd5e405e42d1fbe
akiogitgit <akimaron2828@icloud.com>
2022-12-26 09:16:15 +0900

    feat: 画像追加

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,12 +6,12 @@ type Props = {
   children: React.ReactNode
 }
 
-export const Layout: FC<Props> = ({ title = 'Novel Generater', children }) => {
+export const Layout: FC<Props> = ({ title = 'Novel Generator', children }) => {
   return (
     <div>
       <Head>
         <title>{title}</title>
-        <link rel='icon' href='/dog.png' />
+        <link rel='icon' href='/book_favicon.png' />
       </Head>
 
       <main className='mx-auto my-10 max-w-700px w-90vw'>{children}</main>
