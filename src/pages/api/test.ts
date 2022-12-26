7f1902d53f868df344a6c7d840f9eb9b7ef8db6b
akiogitgit <akimaron2828@icloud.com>
2022-12-26 13:52:42 +0900

    feat: openaiが動くかのテスト

---
+++ src/pages/api/test.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { openai } from '../../libs/openai'
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  const completion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `今からあなたは小説を書きます。タイトルは「ファイアウォールとファミリーマート」です。「剛田たけし」の単語使って作成して下さい。続きが気になり非常に面白く、会話の多い小説にして下さい。`,
+    temperature: 0.6,
+    max_tokens: 300,
+  })
+
+  res.status(200).json({
+    result: completion.data.choices[0].text.split('\n\n').slice(1).join('\n\n'),
+  })
+}
